9a5d44c6fde0031385542cabcbe3e6a2a6f84594
Bibash9543 <140886542+Bibash9543@users.noreply.github.com>
2024-05-13 16:33:59 +0545

    Update index.js

--- react/index.js
+++ react/index.js
@@ -31,3 +31,45 @@ reactDom.render(
   <h1 style={customStyle}>Hello bibash</h1>,
   document.getElementById("root")
 );
+
+
+
+
+//Create a React app from scratch.
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
+import React from "react";
+import reactDom from "react-dom";
+const currentTime = new Date();
+const time = currentTime.getHours();
+console.log(time);
+let greetings;
+let colorChange;
+
+if (time < 12) {
+  greetings = "Good Morning";
+  colorChange = {
+    color: "red",
+  };
+} else if (time < 18) {
+  greetings = "Good Afternoon";
+  colorChange = {
+    color: "green",
+  };
+} else {
+  greetings = "Good evening";
+  colorChange = {
+    color: "blue",
+  };
+}
+
+reactDom.render(
+  <h1 className="heading" style={colorChange}>
+    {greetings}
+  </h1>,
+  document.getElementById("root")
+);
